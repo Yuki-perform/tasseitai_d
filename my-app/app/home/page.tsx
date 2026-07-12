7da7914c9c288b876c3557213d0fc465b2c97387
Yuki-perform <kaiyakutarou@gmail.com>
2026-07-12 22:54:16 +0900

    route.tsに処理を統合

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -1,8 +1,12 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import { getUserDatabaseMap } from "@/lib/notionDatabaseMap";
 import HomeClient from "./HomeClient";
+
+const DEFAULT_DATABASE_IDS = {
+  schedule: "38fa15fd-a3c1-80fa-a200-d99ac64b3409",
+  todo: "38fa15fd-a3c1-80bd-98d9-ddcfe8406a93",
+} as const;
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
@@ -10,13 +14,12 @@ export default async function HomePage() {
     redirect("/login");
   }
 
-  const { databases, unresolved } = await getUserDatabaseMap(session.userId!, session.accessToken);
   return (
     <HomeClient
-      scheduleDatabaseId={databases.schedule}
-      todoDatabaseId={databases.todo}
-      scheduleUnresolved={unresolved.includes("schedule")}
-      todoUnresolved={unresolved.includes("todo")}
+      scheduleDatabaseId={DEFAULT_DATABASE_IDS.schedule}
+      todoDatabaseId={DEFAULT_DATABASE_IDS.todo}
+      scheduleUnresolved={false}
+      todoUnresolved={false}
     />
   );
 }
